3cce8fb5e3daf4cb038a349e2547a407ed458cbe
Riyas Aboobaker <riyas.almoe@gmail.com>
2021-06-07 15:59:02 +0400

    Add project files.

---
+++ ComVisibleCSharpLibrary/PTouch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace ComVisibleCSharpLibrary
+{
+    [Guid("B25E0195-B9F9-42F8-B554-83424A2B24D5")]
+    public interface AllMethods
+    {
+        int IsThisIntMethodVisible();
+        bool IsThisMethodComVisible();
+        string IsThisStringMehtodVisible();
+    }
+
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("16A08691-A17F-4BAE-9D3B-FA5B50F51960")]
+    [ProgId("ComVisibleCSharpLibrary.PTouch")]
+    public class PTouch : AllMethods
+    {
+        public PTouch()
+        {
+
+        }
+
+        public bool IsThisMethodComVisible()
+        {
+            return true;
+        }
+
+        public string IsThisStringMehtodVisible()
+        {
+            return "Yes Visible";
+        }
+
+        public int IsThisIntMethodVisible()
+        {
+            return 1;
+        }
+
+    }
+}
---
+++ ComVisibleVBLibrary/PTouch.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Runtime.InteropServices
+<Guid("042EFBF1-6B28-4C34-A5B7-75997A31E857")>
+Public Interface IAllMethods
+    Function IsThisMethodComVisible() As Boolean
+    Function IsThisStringMethodVisible() As String
+    Function IsThisIntMehtodVisible() As Integer
+
+End Interface
+
+<Guid("10048A53-C6DB-4863-8C80-62DF31C7E43C")>
+<ClassInterface(ClassInterfaceType.None)>
+Public Class PTouch
+    Implements IAllMethods
+
+    Public Function IsThisMethodComVisible() As Boolean Implements IAllMethods.IsThisMethodComVisible
+        Return True
+    End Function
+
+    Public Function IsThisStringMethodVisible() As String Implements IAllMethods.IsThisStringMethodVisible
+        Return "Yes It is Visible from VB.Net Library"
+    End Function
+
+    Public Function IsThisIntMehtodVisible() As Integer Implements IAllMethods.IsThisIntMehtodVisible
+        Return 100
+    End Function
+End Class
